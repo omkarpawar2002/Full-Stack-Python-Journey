4a3dc668d124683dd83802fe161f560e16bddd4d
omkar <omkarsp20@gmail.com>
2026-06-03 12:49:42 +0530

    Learn type() and id() Built in functions

---
+++ 2.0_Variable/built_in_functions.py
@@ -0,0 +1,13 @@
+# type() 
+'''
+    These type() function return the data type of variable.
+'''
+age = 34
+print(age,type(age))
+
+# id() 
+'''
+    These id() function return the memory address of variable
+'''
+name = "lilly"
+print(name,id(name))
