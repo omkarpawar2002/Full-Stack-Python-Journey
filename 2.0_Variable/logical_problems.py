45aeb1be5598e2745e918485fde279941bea69bc
omkar <omkarsp20@gmail.com>
2026-06-03 12:52:17 +0530

    Explore Logical Problems

---
+++ 2.0_Variable/logical_problems.py
@@ -0,0 +1,66 @@
+# Create variables for your name, age, city and print them.
+name = "Rahul"
+age = 34
+city = "Delhi"
+print(name,age,city)
+
+# Store your height in a float variable.
+height = 5.8
+print(height)
+
+# Create a Boolean variable indicating whether you are a student.
+is_student = True
+print(is_student)
+
+# Create three variables in one line.
+num1,num2,num3 = 101,102,103
+print(num1,num2,num3)
+
+# Assign same value to four variables.
+a = b = c = d = "Welcome"
+print(a,b,c,d)
+
+# Reassign a variable three times.
+first_name = "kiran"
+first_name = "neha"
+first_name = "Arun"
+print(first_name)
+
+# Create a variable containing a complex number.
+data = 1 + 7j
+print(data)
+
+# Create a variable with value None and print its type.
+user_details = None
+print(user_details,type(user_details))
+
+# Create variables for a product's name, price and stock status.
+product_name = "Lenove 14.5 Laptop"
+product_price = 80000
+stock_avaialable = True
+print(product_name,product_price,stock_avaialable)
+
+# Print types of 10 different variables.
+student_detail = None
+age = 10
+name = "nayan"
+marks = 78.34
+data = 3 - 4j
+is_student = True
+stu_id = [101,201,301]
+rgb_color = (34,45,56)
+numbers = {1,2,2,3,4,4,5}
+user = {
+    'name':'kelvin',
+    'age':34
+}
+print(student_detail,type(student_detail))
+print(age,type(age))
+print(name,type(name))
+print(marks,type(marks))
+print(data,type(data))
+print(is_student,type(is_student))
+print(stu_id,type(stu_id))
+print(rgb_color,type(rgb_color))
+print(numbers,type(numbers))
+print(user,type(user))
