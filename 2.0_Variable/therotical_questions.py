f178f506f9dcfee1a841a075bc6c4778f2df849c
omkar <omkarsp20@gmail.com>
2026-06-03 12:51:24 +0530

    Practice Theory Questions

---
+++ 2.0_Variable/therotical_questions.py
@@ -0,0 +1,76 @@
+# What is a variable?
+'''
+Variable is a container which is used to store or hold a value so we can reuse it later.
+'''
+
+# Why do we use variables?
+'''
+We used variable because we can reused the values.
+'''
+
+# What is dynamic typing?
+'''
+In Python, variables do not require explicit type declarations. The type is determined from the object assigned to the variable.
+'''
+
+# Write 5 valid variable names.
+'''
+full_name
+name_1
+city 
+isAvailable
+_country
+'''
+
+# Write 5 invalid variable names.
+'''
+full name
+1_name
+#city
+is@available
+country@123
+'''
+
+# What is reassignment?
+'''
+Reassignment means we can provide new value to the existing variable.
+name = "kiran"
+name = "Anju"
+'''
+
+# What is a constant convention?
+'''
+1.In other programming language like c if we want a variable value not be change throught the program so we make the variable constant by using the const keyword.
+2.But here in python there is now way to make a variable constant.
+3.If you want to tell other programmer about email variable will be constant so try to write the variable name in uppercase like EMAIL this. So it's a hint to other try to not modify value but yes we can.
+4.Example :
+            EMAIL = "youremail@gmail.com"
+            PI = 3.14
+'''
+
+# Difference between int and float?
+'''
+Int can store whole number means number without decimal part.
+Float can store decimal number means number with fractional part.
+'''
+
+# What is Boolean type?
+'''
+Boolean type is <class 'bool'> is contain only 2 values either True or False
+'''
+
+# What does None represent?
+'''
+None represents the absence of a value.
+'''
+
+# What does type() do?
+'''
+    These type() function return the data type of variable.
+'''
+
+# What does id() do?
+'''
+    These id() function return the memory address of variable if we are using cpython but 
+    id() returns a unique identity of an object during its lifetime.
+'''
