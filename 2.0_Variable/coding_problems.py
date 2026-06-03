1d6660e7d62a43597189f7fdfcd339284859bc8a
omkar <omkarsp20@gmail.com>
2026-06-03 12:52:41 +0530

    Solve Some Coding Problems

---
+++ 2.0_Variable/coding_problems.py
@@ -0,0 +1,73 @@
+# Create variables for name, age and city.
+name = "nayan"
+age = 32
+city = "mumbai"
+
+# Print all variables.
+print(name,age,city)
+
+# Create one int variable.
+roll = 101
+
+# Create one float variable.
+temperature = 37.5
+
+# Create one complex variable.
+data = 5 - 3j
+
+# Create one Boolean variable.
+is_Login = True
+
+# Create one None variable.
+user_data = None
+
+# Print type of each variable.
+print(type(roll))
+print(type(temperature))
+print(type(data))
+print(type(is_Login))
+print(type(user_data))
+
+# Create three variables using multiple assignment.
+a,b,c = 10,11,12
+print(a,b,c)
+
+# Create four variables with same value.
+num1 = num2 = num3 = num4 = "Welcome"
+print(num1,num2,num3,num4)
+
+# Reassign a variable from 10 to 100.
+num = 10
+print(num)
+num = 100
+print(num)
+
+# Create a variable called PI and assign value 3.14.
+PI = 3.14
+
+# Print the memory ID of a variable.
+print(id(PI))
+
+# Create variables for a student's information.
+stu_details = {
+    'name':'arun',
+    'age':34,
+    'is_student':True
+}
+
+# Create variables for a product's information.
+pro_details = {
+    'name':'Vivo Mobile',
+    'price':45000
+}
+
+# Create variables for an employee's information.
+emp_info = {
+    'name':'haruno',
+    'city':'seoul'
+}
+
+# Create two variables and print their IDs.
+name = "kirti"
+age = 24
+print(id(name),id(age))
