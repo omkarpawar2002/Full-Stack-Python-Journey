4f5efeeb6b88747c98ea94477d6b1eed48715762
omkar <omkarsp20@gmail.com>
2026-06-03 12:53:34 +0530

    Try Mini Project

---
+++ 2.0_Variable/mini_project.py
@@ -0,0 +1,41 @@
+'''
+Here we need to build personal profile Card :
+
+Personal Profile Card
+
+Create variables for:
+
+Name
+Age
+City
+Profession
+Height
+Is Student (True/False)
+
+
+Output like this : 
+----- PROFILE -----
+Name: Siri
+Age: 22
+City: Mumbai
+Profession: Developer
+Height: 5.8
+Student: True
+-------------------
+'''
+
+name = "Advait"
+age = 24
+city = "Mumbai"
+profession = "Developer"
+height = 5.8
+is_student = True
+print("----- PROFILE -----")
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+print("Profession:",profession)
+print("Height:",height)
+print("Student:",is_student)
+print("-------------------")
+
