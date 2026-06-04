ca361c75f16b7d7c7be9066527ff764d30cb201b
omkar <omkarsp20@gmail.com>
2026-06-04 10:18:32 +0530

    Explain Some Theory Questions To Check Understanding

---
+++ 3.0_Type_Casting/therotical_questions.py
@@ -0,0 +1,65 @@
+# What is type casting?
+'''
+Type casting is a process of converting one datatype into another.
+'''
+
+# Difference between implicit and explicit casting?
+'''
+1.When python automatically convert the one data type into another that is known as implicit type casting.
+2.When programmer convert the one data type into another by using python built-in functions that is known as explicit type casting.
+'''
+
+# What does int() do?
+'''
+int() function is used to convert the data into integer
+'''
+
+# What does float() do?
+'''
+float() function is used to convert data into floating point value.
+'''
+
+# What does str() do?
+'''
+str() function is used to convert data into string.
+'''
+
+# What does bool() do?
+'''
+bool() function is used to convert the data into boolean type.
+'''
+
+# What does input() return?
+'''
+input() function reads input from the user and returns it as a string.
+'''
+
+# Why do we convert user input?
+'''
+we convert the user input because different data types behave differently
+'''
+
+# What is the purpose of print()?
+'''
+To display the output on screen
+'''
+
+# What is sep parameter?
+'''
+By default multiple objects seperated by space but we can seperate them by using sep parameter inside print() function
+'''
+
+# What is end parameter?
+'''
+By default print the object in next line but we can print on same line also by using end parameter.
+'''
+
+# How do you take multiple inputs?
+'''
+# By asking multiple input :
+
+name = input("Enter your name : ")
+city = input("Enter your city : ")
+print(name)
+print(city)
+'''
