aaa0ddfd582a5f69b9e63d86e031b1b02f1ac4b6
omkar <omkarsp20@gmail.com>
2026-06-05 20:48:11 +0530

    Explore Assignment Operators

---
+++ 4.0_Operators/assignment_operators.py
@@ -0,0 +1,77 @@
+# Assignment Operators :
+'''
+Assignment Operators are used to assign the values to Variables.
+
+Different Types of Assignment Operators :
+    1.Assignment [ = ]
+    2.Addition Assignment [ += ]
+    3.Subtraction Assignment [ -= ]
+    4.Multiplication Assignment [ *= ]
+    5.Division Assignment [ /= ]
+    6.Floor Division Assignment [ //= ]
+    7.Modulus Assignment [ %= ]
+    8.Exponent Assignment [ **= ]
+'''
+
+# Assignment [ = ]
+'''
+Assignment [ = ] operator is used to assign the RHS value to the LHS of variable.
+'''
+number = 34
+print(number)
+
+# Addition Assignment [ += ]
+'''
+Addition Assignment [ += ] is used to add first then assign to the variable.
+'''
+number = 10
+number += 5    # number = number + 5
+print(number)
+
+# Subtraction Assignment [ -= ]
+'''
+Subtraction Assignment [ -= ] is used to subtract first then assign to the variable.
+'''
+number = 10
+number -= 5    # number = number - 5
+print(number)
+
+# Multiplication Assignment [ *= ]
+'''
+Multiplication Assignment [ *= ] is used to multiply first then assign to the variable.
+'''
+number = 10
+number *= 5    # number = number * 5
+print(number)
+
+# Division Assignment [ /= ]
+'''
+Division Assignment [ /= ] is used to division first then assign floating point value to the variable.
+'''
+number = 10
+number /= 5    # number = number / 5
+print(number)
+
+# Floor Division Assignment [ //= ]
+'''
+Floor Division Assignment [ //= ] is used to division first then assign rounded backword integer quotient value to the variable.
+'''
+number = 10
+number //= 5    # number = number // 5
+print(number)
+
+# Modulus Assignment [ %= ]
+'''
+Modulus Assignment [ %= ] return remainder and then assign to variable.
+'''
+number = 10
+number %= 5    # number = number % 5
+print(number)
+
+# Exponent Assignment [ **= ]
+'''
+Exponent Assignment [ **= ] return power of the value and assign to the variable.
+'''
+number = 10
+number **= 5    # number = number ** 5
+print(number)
