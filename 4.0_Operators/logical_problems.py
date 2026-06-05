353b05e4b53a400b310ac495eaa2bbe6137c44a7
omkar <omkarsp20@gmail.com>
2026-06-05 20:50:28 +0530

    Solve Some Logical Problems

---
+++ 4.0_Operators/logical_problems.py
@@ -0,0 +1,14 @@
+# Calculate square of a number.
+number = 4
+print(number ** 2)
+
+# Calculate cube of a number.
+number = 4
+print(number ** 3)
+
+# Find total cost of 5 items.
+total_cost = 100
+print(total_cost * 5)
+
+# Check remainder when 25 is divided by 4.
+print(25 % 4)
