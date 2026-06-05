8a5a28159588b77212df4683a996e216ea18c1c5
omkar <omkarsp20@gmail.com>
2026-06-05 20:48:32 +0530

    Explore Logical Operators

---
+++ 4.0_Operators/logical_operators.py
@@ -0,0 +1,28 @@
+# Logical Operators :
+'''
+Logical operators are used to check multiple conditions and return a Boolean result or an Expression.
+Logical Operators are work with comparison operators to check the conditions.
+
+There are 3 types of logical operators : 
+    1.and 
+    2.or 
+    3.not
+'''
+
+# and 
+'''
+and operator return True if both the conditions are True and if any one condition is false then it return False
+'''
+print(10 > 4 and 19 > 20)
+
+# or 
+'''
+or operator return True if any one of the condition is True and it return False only when both the conditions are False
+'''
+print(10 > 4 or 19 > 20)
+
+# not
+'''
+not operator return True if the condition is False and return False if condition is True.
+'''
+print(not(False))
