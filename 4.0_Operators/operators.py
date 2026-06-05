42b39e9ecedc731c06f5bdb04181da7c34bc8a45
omkar <omkarsp20@gmail.com>
2026-06-05 20:45:53 +0530

    Explore Different Operators

---
+++ 4.0_Operators/operators.py
@@ -0,0 +1,17 @@
+# Operators
+'''
+Operators are special symbol which are used to perform operations on operands/variables and values.
+
+Example : 10 + 2 
+Here 10 and 2 are operands and + is an operator.
+'''
+
+# Different Types Of Operators In Python
+'''
+    1.Arithmatic Operators
+    2.Comparison / Relational Operators
+    3.Logical Operators
+    4.Assignment Operators
+    5.Membership Operators
+    6.Identity Operators
+'''
