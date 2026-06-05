e849b97d77844d0584274b4b9611ce34631b0c28
omkar <omkarsp20@gmail.com>
2026-06-05 20:47:49 +0530

    Explore Comparison Operators

---
+++ 4.0_Operators/comparison_operators.py
@@ -0,0 +1,56 @@
+# Comparison Operators :
+'''
+Comparison Operators is used to compare two values and return a boolean result either True or False.
+
+Comparison Operators :
+    1.Equal to [ == ]
+    2.Not Equal to [ != ]
+    3.Greater than [ > ]
+    4.Greater than or equal to [ >= ]
+    5.Less than [ < ]
+    6.Less than or equal to [ <= ]
+'''
+
+# Equal to [ == ]
+'''
+Equal to [ == ] operator return True if both LHS and RHS are equal.Otherwise it return False
+'''
+print(10 == 10)
+print(10 == 34)
+
+# Not Equal to [ != ]
+'''
+Not Equal to [ != ] operator return True if both LHS and RHS are not equal.Otherwise it return False
+'''
+print(10 != 10)
+print(10 != 5)
+
+# Greater than [ > ]
+'''
+Greater than [ > ] operator return True if LHS is greater than RHS.Otherwise it return False
+'''
+print(10 > 3)
+print(10 > 39)
+
+# Greater than equal to [ >= ]
+'''
+Greater than equal to [ >= ] operator return True if LHS is greater than or equal to RHS.Otherwise it return False
+'''
+print(10 >= 3)
+print(10 >= 39)
+print(10 >= 10)
+
+# Less than [ < ]
+'''
+Less than [ < ] operator return True if LHS is less than RHS.Otherwise it return False
+'''
+print(10 < 3)
+print(10 < 39)
+
+# Less than equal to [ <= ]
+'''
+Less than equal to [ <= ] operator return True if LHS is less than or equal to RHS.Otherwise it return False
+'''
+print(10 <= 3)
+print(10 <= 39)
+print(10 <= 10)
