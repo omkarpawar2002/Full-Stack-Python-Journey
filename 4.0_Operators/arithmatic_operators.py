eb310ed7f9cf0430795e84f2a7227a0cc0647b0b
omkar <omkarsp20@gmail.com>
2026-06-05 20:47:19 +0530

    Explore Arithmatic Operators

---
+++ 4.0_Operators/arithmatic_operators.py
@@ -0,0 +1,69 @@
+# Arithmatic Operators :
+'''
+Arithmatic Operators are used to perform mathmatical calculations like addition,subtraction,multiplication or division like these :
+
+Arithmatic Operators :
+    1.Addition [ + ]
+    2.Subtraction [ - ]
+    3.Multiplication [ * ]
+    4.Divition [ / ]
+    5.Floor Division [ // ]
+    6.Modulus [ % ]
+    7.Exponent [ ** ]
+'''
+
+# Addition [ + ]
+'''
+Addition [ + ] operator is used to add two numbers.
+'''
+num1 = 10
+num2 = 34
+print(num1 + num2)
+
+# Subtraction [ - ]
+'''
+Subtraction [ - ] operator is used to subtract second number from first number.
+'''
+num1 = 100
+num2 = 1200
+print(num2 - num1)
+
+# Multiplication [ * ]
+'''
+Multiplication [ * ] operator is used to multiply two numbers 
+'''
+num1 = 10
+num2 = 4
+print(num1 * num2)
+
+# Division [ / ]
+'''
+Division [ / ] operator is used to do division and return floating point quotient value.
+'''
+num1 = 10
+num2 = 3
+print(num1 / num2)
+
+# Floor Division [ // ]
+'''
+Floor Division [ // ] operator is used return rounded backword integer quotient value.
+'''
+num1 = 10
+num2 = 3
+print(num1 // num2)
+
+# Modulus [ % ]
+'''
+Modulus [ % ] operator is used to return remainder value.
+'''
+num1 = 10
+num2 = 3
+print(num1 % num2)
+
+# Exponent [ ** ]
+'''
+Exponent [ ** ] operator is used to return power of value
+'''
+num1 = 10
+num2 = 3
+print(num1 ** num2)
