66e57094e2918a33f77ea3e1145473af7ac92fd3
omkar <omkarsp20@gmail.com>
2026-06-05 20:48:53 +0530

    Explore Membership Operators

---
+++ 4.0_Operators/membership_operators.py
@@ -0,0 +1,24 @@
+# Membership Operators :
+'''
+Membership operators is used to check whether the value found in collection.
+
+There are 2 types of membership operator :
+    1.in
+    2.not in
+'''
+
+# in
+'''
+in operator return True if the value occurs in specify sequence otherwise it return False
+'''
+numbers = [10,20,30,40,50]
+print(10 in numbers)
+print(102 in numbers)
+
+# not in
+'''
+not in operator return True if the value does not occurs in specify sequence otherwise it return False
+'''
+numbers = [10,20,30,40,50]
+print(10 not in numbers)
+print(102 not in numbers)
