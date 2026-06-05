7f76be1c953ea87e01fdd9cf730272008d718c4c
omkar <omkarsp20@gmail.com>
2026-06-05 20:50:49 +0530

    Solve Some Coding Problems

---
+++ 4.0_Operators/coding_problems.py
@@ -0,0 +1,16 @@
+# Calculate total price of products.
+product_1 = 450
+product_2 = 380
+total_price = product_1 + product_2
+print(total_price)
+
+# Calculate average of two numbers.
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+total = num1 + num2
+print("Average of 2 numbers : ",total/2)
+
+# Compare two floating-point numbers.
+num1 = 45.5
+num2 = 45.5
+print(num1 == num2)
