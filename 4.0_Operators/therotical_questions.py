8947bacdf30b1447df7a02818b559a6a778327f2
omkar <omkarsp20@gmail.com>
2026-06-05 20:50:00 +0530

    Practice Theory Questions

---
+++ 4.0_Operators/therotical_questions.py
@@ -0,0 +1,68 @@
+# What is an operator?
+'''
+Operators are special symbol which are used to perform operations on operands/variables and values.
+'''
+
+# What is an arithmetic operator?
+'''
+Arithmatic Operators are used to perform mathmatical calculations like addition,subtraction,multiplication,division,floor division,modulus or exponenet.
+'''
+
+# Difference between / and //?
+'''
+1.Division [ / ] will return floating point quotient value.
+2.Floor Division [ // ] will return rounded backword integer quotient value.
+'''
+
+# What is a comparison operator?
+'''
+Comparison Operators is used to compare two values and return a boolean result either True or False.
+'''
+
+# What type of value do comparison operators return?
+'''
+Comparison operators return a boolean result which is True or False only.
+'''
+
+# What is the difference between = and ==?
+'''
+1. [ = ] is an assignment operator which is responsible for assigning RHS value to the LHS of variable.
+2. [ == ] is an equality operator which return True if both LHS and RHS are equal otherwise they return False.
+'''
+
+# Give two real-life examples of comparison.
+'''
+Suppose we want to login in website so for that we check whether username or password correct or not so that time we used comparison operator.
+'''
+
+# What is a logical operator?
+'''
+Logical operators are used to check multiple conditions and return a Boolean result or an Expression.
+'''
+
+# Why are logical operators used?
+'''
+we used logical operators to check the multiple conditions while using comparison operator.
+'''
+
+# What is an assignment operator?
+'''
+Assignment Operators are used to assign the values to Variables.
+Here = [ Equal to ] is an assignment operator.
+'''
+
+# What is an identity operator?
+'''
+Identity Operators are used to check whether both objects / variable point to the same memory location or not.
+'''
+
+# Difference between == and is?
+'''
+1. [ == ] Equal to operator is used to check whether both objects/values are equal or not.
+2. [ is ] operator is used to check the identity number of both objects.
+'''
+
+# What is a membership operator?
+'''
+Membership operators is used to check whether the value found in collection.
+'''
