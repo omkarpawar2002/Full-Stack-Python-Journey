9a45fd91e4d35dabe60f2b4e08502f942a54bfb5
omkar <omkarsp20@gmail.com>
2026-06-06 20:54:10 +0530

    Practice Some Therotical Concepts

---
+++ 5.0_Conditional_statements/theory_questions.py
@@ -0,0 +1,104 @@
+# What is a conditional statement?
+"""
+Conditional Statments are used to manipulate the flow of our program execution.
+"""
+
+# Why do we use conditional statements?
+"""
+To manipulate the program flow means depending on particular condition the code will get executed that all things possible by using conditional statements.
+"""
+
+# What is the purpose of an if statement?
+'''
+When we want to check only one condition then we used if statement.
+'''
+
+# When does an if block execute?
+'''
+The if block will be executed only when the condition specified after the if keyword is True otherwise if will be skipped.
+'''
+
+# What happens when an if condition is False?
+'''
+If block will be skipped and next statement executed.
+'''
+
+# What is the purpose of else?
+'''
+If condition is false then if block will be skipped and corresponding else block executed.
+'''
+
+# What is the purpose of elif?
+'''
+when we want to check multiple conditions then we used elif blocks.
+'''
+
+# Can we use multiple elif statements?
+'''
+Yes
+'''
+
+# What is a nested if?
+'''
+When if inside another if known as nested if in python.
+'''
+
+# Why do we use logical operators in conditions?
+'''
+Logical operators are used in conditions because they allow us to check the multiple conditions.
+'''
+
+# What does and do in a condition?
+"""
+and is used to check all condition are True or not.
+"""
+
+# What does or do in a condition?
+"""
+or is used to check any one of the condition are True or not.
+"""
+
+# What does not do in a condition?
+'''
+not reverse the condition means if it is True it becomes False and vice versa.
+'''
+
+# What is shorthand if?
+'''
+When we have single statement in if block then we can write it on same line but if we have complex code then we write in normal way in multiple line.
+'''
+
+# When should shorthand if be used?
+'''
+When we have single statement in if block then we can write it on same line.
+'''
+
+# What is a ternary operator?
+'''
+1.Python does not use the ?: ternary operator syntax found in some other languages.
+2.Instead, it provides a conditional expression:
+value_if_true if condition else value_if_false
+'''
+
+# Why is a ternary operator useful?
+'''
+Ternary expressions are used to choose between two values based on a condition
+'''
+
+# Difference between if else and ternary operator?
+'''
+The main difference between if-else and ternary operator are when we have simple logic then check via ternary operator and when we have complex code that time go with if else block.
+'''
+
+# Give three real-life examples of conditional statements.
+"""
+# When we login into website so we require this things to make sure the correct data : 
+
+
+username = input("Enter your username : ")
+password = input("Enter your password : ")
+if(username == "admin" and password == "admin@123"):
+    print("Login Successful!!")
+else:
+    print("Login Failed!!")
+"""
