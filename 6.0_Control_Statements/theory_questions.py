0e211087c5bcfd3ef1d44242493ecfe370fdc534
omkar <omkarsp20@gmail.com>
2026-06-15 13:25:49 +0530

    Practice Some Theory Questions

---
+++ 6.0_Control_Statements/theory_questions.py
@@ -0,0 +1,113 @@
+# What is a loop?
+'''
+Loop is way to execute certain thing again and again.
+'''
+
+# Why do we use loops?
+'''
+We used loop to execute certain code again and again and to avoid repetative code.
+'''
+
+# What are the two main loops in Python?
+'''
+for loop and while loop.
+'''
+
+# What is a for loop?
+'''
+when we know exact number of time iteration is required then we used for loop.
+'''
+
+# What is a while loop?
+'''
+When we don't know exact number of time the iteration is required then we used while loop.
+The loop keep running until the specify condition is True.
+'''
+
+# When should you use a for loop?
+'''
+when we know exact number of time iteration is required then we used for loop.
+'''
+
+# When should you use a while loop?
+'''
+When we don't know exact number of time the iteration is required then we used while loop.
+'''
+
+# What is iteration?
+"""
+Iteration means one execution (one cycle) of a loop.
+
+for i in range(3):
+    print(i)
+
+The loop performs 3 iterations.
+"""
+
+# How do you iterate through a string?
+'''
+# So here st is a iterable object string and item is a single element that are iterate over iterable object 
+
+st = "welcome"
+for item in st:
+    print(item)
+'''
+
+# How do you iterate through a list?
+'''
+# So here li is a iterable object list and item is a single element that are iterate over iterable object 
+
+li = [10,20,30,40,50]
+for item in li:
+    print(item)
+'''
+
+# What does range() do?
+'''
+range() is used to generate sequence of numbers.
+'''
+
+# Does range(5) include 5?
+'''
+No stop value not included and range generate number upto stop_value - 1
+'''
+
+# What does range(1, 6) produce?
+'''
+It print number from 1 to 5 and 6 excluded.
+'''
+
+# What is an infinite loop?
+'''
+Infinite loop means loop running continusly without stopping.
+'''
+
+# Why are infinite loops dangerous?
+'''
+Infinite loops are dangerous because they keep consuming CPU time and system resources forever unless stopped manually. They can make a program unresponsive.
+'''
+
+# What is a loop condition?
+'''
+Loop condition means we provide a condition so the loop will stop when certain condition becomes false.
+'''
+
+# What does break do?
+'''
+break keyword is used when we immediately wants to exits from the loop.
+'''
+
+# What does continue do?
+'''
+Continue keyword is used when we want to skip the current iteration and jump on the next iteration.
+'''
+
+# What does pass do?
+'''
+pass keyword is used when our program is syntactically correct but we don't want to implement or not do anything in block so we used pass.
+'''
+
+# What is a nested loop?
+'''
+When loop inside another loop known as a nested loop in python.
+'''
